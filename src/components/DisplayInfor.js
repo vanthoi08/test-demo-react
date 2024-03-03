2ce4b0644725eb5bc0ff60aea9d9dd7087149958
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-03-03 11:57:02 +0900

    video 38

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,61 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss'
 import logo from './../logo.svg';
 
 
 // stateless vs statefull
-// class DisplayInfor extends React.Component {
-
-//     render() {
-//         console.log('>>> call me render')
-//         // destructuring array/object
-//         const { listUsers } = this.props;
-//         // console.log(listUsers);
-//         // console.table(listUsers);
-//         // props => viết tắt properties
-//         return (
-//             <div className="display-infor-container">
-
-
-//                 {true &&
-//                     // Fragment
-//                     <>
-//                         {listUsers.map((user, index) => {
-//                             return (
-//                                 // convert string to number javascript thêm dấu + ở phía trước
-//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"} >
-//                                     <div>
-//                                         <div>My name's {user.name} </div>
-//                                         <div>My age's {user.age} </div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => { this.props.handleDeleteUser(user.id) }}>Delete</button>
-//                                     </div>
-//                                     <hr />
-//                                 </div>
-
-//                             )
-//                         })}
-
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
-
-
 const DisplayInfor = (props) => {
     const { listUsers } = props;
     // Bien            // ham cap nhat giá trị
     const [isShowHideListUser, setShowHideListUser] = useState(true);
+    // Destructuring assignment
     //  tuowng đương
     // this.state = {
     //     isShowHideListUser: true
     // }
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser);
+        // this.state = {
+        //     isShowHideListUser: true
+        // }
     }
+
+    console.log('>>> call me render')
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0) {
+                alert('You deleted all the users');
+            }
+            console.log('>>> call me useEffect ')
+        }, [listUsers]
+    );
+
+    useEffect(
+        () => {
+            if (listUsers.length === 0) {
+                alert('You deleted all the users');
+            }
+            console.log('>>> call me useEffect ')
+        }, [abc]
+    );
+
+
+
 
     return (
         <div className="display-infor-container">
