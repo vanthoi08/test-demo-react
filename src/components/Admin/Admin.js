63ae3ca49c375f48a82fdf8664863aa07e5140e5
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-03-09 21:42:34 +0900

    video 59

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,9 +1,11 @@
 import SideBar from "./SideBar";
 import './Admin.scss';
 import { FaBars } from 'react-icons/fa';
-import { FcPlus } from "react-icons/fc";
+
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
 
@@ -19,9 +21,17 @@ const Admin = (props) => {
                 <div className="admin-main">
                     <Outlet />
                 </div>
-
-
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+            />
         </div>
     )
 }
--- src/components/Admin/Content/ModalCreateUses.js
+++ src/components/Admin/Content/ModalCreateUses.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -35,18 +36,32 @@ const ModalCreateUser = (props) => {
         }
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handSubmitCreateUser = async () => {
         // validate
 
-        // cal api
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     userImage: image
-        // }
-        // console.log(data);
+        const isValidEmail = validateEmail(email);
+
+        if (!isValidEmail) {
+            toast.error('Invalid email');
+            // toast.success("hhhh");
+            // toast.info("hhhh");
+            return;
+        }
+
+        if (!password) {
+            toast.error('Invalid password');
+            return;
+        }
+
+        // submit data
         const data = new FormData();
         data.append('email', email);
         data.append('password', password);
@@ -55,7 +70,15 @@ const ModalCreateUser = (props) => {
         data.append('userImage', image);
 
         let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-        console.log(">>> check res : ", res);
+        // console.log(">>> check res : ", res.data);
+        if (res.data && res.data.EC === 0) {
+            // EM là EM "Create a new participant succeed"
+            toast.success(res.data.EM);
+            handleClose();
+        }
+        if (res.data && res.data.EC !== 0) {
+            toast.error(res.data.EM);
+        }
     }
     return (
         <>
