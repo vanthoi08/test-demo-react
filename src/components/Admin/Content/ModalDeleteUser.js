72592fdaa03284798e677c42b3722d115ea98b08
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-03-10 17:32:27 +0900

    video 67

--- src/components/Admin/Content/ModalDeleteUser.js
+++ src/components/Admin/Content/ModalDeleteUser.js
@@ -1,14 +1,27 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { deleteUser } from '../../../services/apiService';
+import { toast } from 'react-toastify';
 
 const ModalDeleteUser = (props) => {
     const { show, setShow, dataDelete } = props;
 
     const handleClose = () => setShow(false);
+    // async,await xử lý bất đồng bộ
+    const handleSubmitDeleteUser = async () => {
+        let data = await deleteUser(dataDelete.id);
+        console.log("componemt res", data);
 
-    const handleSubmitDeleteUser = () => {
-        alert('me');
+        if (data && data.EC === 0) {
+            // EM là EM "Create a new participant succeed"
+            toast.success(data.EM);
+            handleClose();
+            await props.fetchListUsers();
+        }
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
+        }
     }
     return (
         <>
