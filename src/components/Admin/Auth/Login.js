a2247cabba74a18ce98a1af957552493515d783a
vanthoi08 <156212850+vanthoi08@users.noreply.github.com>
2024-03-20 10:39:17 +0900

    video 70

--- src/components/Admin/Auth/Login.js
+++ src/components/Admin/Auth/Login.js
@@ -1,17 +1,37 @@
 import { useState } from 'react';
 import './login.scss';
+import { useNavigate } from "react-router-dom";
+import { postLogin } from '../../../services/apiService';
+
+import { toast } from 'react-toastify';
 
 const Login = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const handleLogin = () => {
-        alert('login');
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        // validate
+
+        // submit apis
+
+        let data = await postLogin(email, password);
+        if (data && data.EC === 0) {
+
+            toast.success(data.EM);
+            navigate('/');
+
+        }
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
+        }
     }
 
     return (
         <div className="login-container">
             <div className='header'>
-                Don't have an account yet?
+                <span>Don't have an account yet?</span>
+                <button>Sign up</button>
             </div>
             <div className='title col-4 mx-auto'>
                 Hoi Dan It
@@ -41,11 +61,15 @@ const Login = (props) => {
                 </div>
                 <span className='forgot-password'>Forgot password ?</span>
                 <div>
-
                     <button
                         className='btn-submit'
                         onClick={() => handleLogin()}
                     >Login to HoiDanIT</button>
+                </div>
+                <div className="text-center">
+                    <span className="back" onClick={() => { navigate('/') }}>
+                        &#60;&#60;Go to Homepage
+                    </span>
                 </div>
             </div>
         </div>
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -33,4 +33,17 @@ const getUserWithPaginate = (page, limit) => {
     return axios.get(`api/v1/participant?page=${page}&limit=${limit}`);
 }
 
-export { postCreateNewUser, getAllUsers, putUpdateUser, deleteUser, getUserWithPaginate }
+const postLogin = (userEmail, userPassword) => {
+    return axios.post(`/api/v1/login`,
+        { email: userEmail, password: userPassword }
+    );
+}
+
+// const postLogin1 = (email, password) => {
+//     return axios.post(`/api/v1/login`,
+//         { email, password}
+//     );
+// }
+
+
+export { postCreateNewUser, getAllUsers, putUpdateUser, deleteUser, getUserWithPaginate, postLogin }
